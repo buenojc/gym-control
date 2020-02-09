d0e63ef66a0c296f8a35c9dc6e7c05907ea90087
Julio Cesar Bueno <buenojc@outlook.com>
2020-02-09 20:27:42 -0200

    Server Running

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const routes = require("./routes");
+const server = express();
+
+server.listen(5050, function() {
+  console.log("Server is Running");
+});
+
+// Express configuration
+
+server.set("view engine", "njk");
+server.use(routes);
+server.use(express.static("public"));
+
+// Nunjucks configuration
+
+nunjucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true
+});
